0c3adf2e14e0d7deb69c7b02cca08c24692bf2a6
sadik-mahdi <sadikmahadi06@gmail.com>
2026-05-04 19:27:35 +0600

    fix: prevent MongoClient from crashing on Vercel build due to undefined env var

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -2,7 +2,7 @@
   import { MongoClient } from "mongodb";
   import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
-  const client = new MongoClient(process.env.AUTH_DB_URI);
+  const client = new MongoClient(process.env.AUTH_DB_URI || "mongodb://localhost:27017/dummy");
   const db = client.db('borrow-book');
 
   export const auth = betterAuth({
