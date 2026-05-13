6d4e0f33e321c0a011abf614df5216797ac8583b
sadik-mahdi <sadikmahadi06@gmail.com>
2026-05-13 11:20:29 +0600

    fixed issue

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -2,7 +2,7 @@ import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
-const client = new MongoClient(process.env.AUTH_DB_URI || "mongodb://localhost:27017/dummy");
+const client = new MongoClient(process.env.MONGODB_URI);
 const db = client.db('borrow-book');
 
 export const auth = betterAuth({
